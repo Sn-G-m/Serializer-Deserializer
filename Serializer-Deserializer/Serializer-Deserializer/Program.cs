52d028f6ab84f49b3488f3a5e7f86ba7107097d6
Sn-G-m <112201012@smail.iitpkd.ac.in>
2025-10-19 23:48:20 +0530

    Interfaces for serializer and datastructures, and initial setup

---
+++ Serializer-Deserializer/Serializer-Deserializer/Program.cs
@@ -0,0 +1,19 @@
+﻿using DataStructures;
+using SerDser;
+
+namespace program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataStructure binarySearchTree = new BinarySearchTree();
+            ISerDser serializerDeserializer = new BstSerializer();
+
+            List<int> nodeValues = new List<int> { 7, 8, 3, 4, 9 };
+            
+            binarySearchTree.CreateDsFromList(nodeValues);
+            binarySearchTree.PrintDs();
+        }
+    }
+}
