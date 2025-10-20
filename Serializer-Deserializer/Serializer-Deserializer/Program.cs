3c3cf3e78599ad8442de5f068027265ac2bd61bb
Sn-G-m <112201012@smail.iitpkd.ac.in>
2025-10-20 23:26:48 +0530

    Linked list and Bst serializer-deserializer implemented, along with pretty printing

--- Serializer-Deserializer/Serializer-Deserializer/Program.cs
+++ Serializer-Deserializer/Serializer-Deserializer/Program.cs
@@ -7,13 +7,47 @@ namespace program
     {
         static void Main(string[] args)
         {
+            // Datastructures
             IDataStructure binarySearchTree = new BinarySearchTree();
-            ISerDser serializerDeserializer = new BstSerializer();
+            IDataStructure linkedList = new LinkedList();
 
+            // Serializer-Deserializer
+            ISerDser serializerDeserializer = new BstSerializer();
+            ISerDser serializerLL = new LlSerializer();
+
+            // Variable for holding serialized data.
+            byte[] serializedData;
+
+            // Node values as input for data structure creation
             List<int> nodeValues = new List<int> { 7, 8, 3, 4, 9 };
             
+            // Create and print newly created structures.
             binarySearchTree.CreateDsFromList(nodeValues);
+            linkedList.CreateDsFromList(nodeValues);
+
+            Console.WriteLine("Binary Search Tree...\n");
             binarySearchTree.PrintDs();
+            
+            Console.WriteLine("Linked List...\n");
+            linkedList.PrintDs();
+
+            serializedData = serializerDeserializer.Serializer(binarySearchTree);
+
+            // Stores deserialized bytestreams.
+            IDataStructure decodedBinarySearchTree = new BinarySearchTree();
+            IDataStructure decodedLinkedList = new LinkedList();
+
+            decodedBinarySearchTree = (BinarySearchTree) serializerDeserializer.Deserializer(serializedData);
+
+            serializedData = serializerLL.Serializer(linkedList);
+            decodedLinkedList = (LinkedList)serializerLL.Deserializer(serializedData);
+
+            // Print the decoded data to check correctness.
+            Console.WriteLine("\nDeserialized Binary Search Tree...\n");
+            decodedBinarySearchTree.PrintDs();
+
+            Console.WriteLine("\nDeserialize Linked List...\n");
+            decodedLinkedList.PrintDs();
         }
     }
 }
